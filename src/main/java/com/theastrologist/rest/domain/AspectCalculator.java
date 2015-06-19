473b767b0671f32d70fcd4b43cc35875a058d52e
Samy Amirou <samyamirou@gmail.com>
2015-06-19 22:20:05 +0200

    Test + code synastrie

--- src/main/java/com/theastrologist/rest/domain/AspectCalculator.java
+++ src/main/java/com/theastrologist/rest/domain/AspectCalculator.java
@@ -16,30 +16,7 @@ public class AspectCalculator {
      */
     public Map<Planet, Map<Planet, AspectPosition>> createAspectsForSkyPosition(SkyPosition skyPosition) {
 
-        Map<Planet, Map<Planet, AspectPosition>> aspectMapForPlanet = new HashMap<Planet, Map<Planet, AspectPosition>>();
-
-        for (Planet planet : Planet.values()) {
-            PlanetPosition planetPosition = skyPosition.getPlanetPosition(planet);
-            Map<Planet, AspectPosition> aspectMap;
-            if (!aspectMapForPlanet.containsKey(planet)) {
-                aspectMap = new HashMap<Planet, AspectPosition>();
-                aspectMapForPlanet.put(planet, aspectMap);
-            } else {
-                aspectMap = aspectMapForPlanet.get(planet);
-            }
-
-            for (Planet planetComparison : Planet.values()) {
-                if (planet != planetComparison) {
-                    PlanetPosition planetComparisonPosition = skyPosition.getPlanetPosition(planetComparison);
-                    AspectPosition aspectPosition = AspectPosition.createAspectPosition(planet, planetComparison, planetPosition, planetComparisonPosition);
-                    if (aspectPosition != null) {
-                        aspectMap.put(planetComparison, aspectPosition);
-                    }
-                }
-            }
-        }
-
-        return aspectMapForPlanet;
+        return createAspectsForComparison(skyPosition, skyPosition);
     }
 
     /**
