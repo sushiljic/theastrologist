18a4a0ac47a4dee5f9a6bdc2633b72e3da14f7da
samyamirou <samyamirou@gmail.com>
2015-07-25 21:30:28 +0200

    Ajout maitrises et exaltations des signes

--- src/main/java/com/theastrologist/rest/domain/Sign.java
+++ src/main/java/com/theastrologist/rest/domain/Sign.java
@@ -7,26 +7,51 @@ import java.util.List;
  * Created by SAM on 16/11/2014.
  */
 public enum Sign {
-    BELIER(1),
-    TAUREAU(2),
-    GEMEAUX(3),
-    CANCER(4),
-    LION(5),
-    VIERGE(6),
-    BALANCE(7),
-    SCORPION(8),
-    SAGITTAIRE(9),
-    CAPRICORNE(10),
-    VERSEAU(11),
-    POISSONS(12);
-    private int signNumber;
+    BELIER(1, new Planet[]{Planet.MARS, Planet.PLUTON}, new Planet[]{Planet.SOLEIL}, new Planet[]{Planet.VENUS}, new Planet[]{Planet.SATURNE, Planet.URANUS}),
+    TAUREAU(2, new Planet[]{Planet.VENUS}, new Planet[]{Planet.LUNE}, new Planet[]{Planet.MARS, Planet.PLUTON}, new Planet[]{Planet.SATURNE, Planet.URANUS}),
+    GEMEAUX(3, new Planet[]{Planet.MERCURE}, new Planet[]{Planet.VENUS}, new Planet[]{Planet.JUPITER, Planet.NEPTUNE}, new Planet[]{Planet.MERCURE, Planet.NEPTUNE}),
+    CANCER(4, new Planet[]{Planet.LUNE}, new Planet[]{Planet.JUPITER}, new Planet[]{Planet.SATURNE, Planet.URANUS}, new Planet[]{Planet.MARS, Planet.PLUTON}),
+    LION(5, new Planet[]{Planet.SOLEIL}, new Planet[]{Planet.MARS, Planet.PLUTON}, new Planet[]{Planet.SATURNE, Planet.URANUS}, new Planet[]{Planet.JUPITER}),
+    VIERGE(6, new Planet[]{Planet.MERCURE}, new Planet[]{Planet.MERCURE, Planet.NEPTUNE}, new Planet[]{Planet.JUPITER, Planet.NEPTUNE}, new Planet[]{Planet.VENUS}),
+    BALANCE(7, new Planet[]{Planet.VENUS}, new Planet[]{Planet.SATURNE, Planet.URANUS}, new Planet[]{Planet.MARS, Planet.PLUTON}, new Planet[]{Planet.SOLEIL}),
+    SCORPION(8, new Planet[]{Planet.MARS, Planet.PLUTON}, new Planet[]{Planet.SATURNE, Planet.URANUS}, new Planet[]{Planet.VENUS}, new Planet[]{Planet.LUNE}),
+    SAGITTAIRE(9, new Planet[]{Planet.JUPITER, Planet.NEPTUNE}, new Planet[]{Planet.MERCURE, Planet.NEPTUNE}, new Planet[]{}, new Planet[]{Planet.VENUS}),
+    CAPRICORNE(10, new Planet[]{Planet.SATURNE, Planet.URANUS}, new Planet[]{Planet.MARS, Planet.PLUTON}, new Planet[]{Planet.LUNE}, new Planet[]{Planet.JUPITER}),
+    VERSEAU(11, new Planet[]{Planet.SATURNE, Planet.URANUS}, new Planet[]{Planet.JUPITER}, new Planet[]{Planet.SOLEIL}, new Planet[]{Planet.MARS, Planet.PLUTON}),
+    POISSONS(12, new Planet[]{Planet.JUPITER, Planet.NEPTUNE}, new Planet[]{Planet.VENUS}, new Planet[]{Planet.MERCURE}, new Planet[]{Planet.MERCURE, Planet.NEPTUNE});
 
-    Sign(int signNumber) {
+    private int signNumber;
+    private final Planet[] masterPlanets;
+    private final Planet[] exaltedPlanets;
+    private final Planet[] exilPlanets;
+    private final Planet[] chutePlanets;
+
+    Sign(int signNumber, Planet[] masterPlanets, Planet[] exaltedPlanets, Planet[] exilPlanets, Planet[] chutePlanets) {
         this.signNumber = signNumber;
+        this.masterPlanets = masterPlanets;
+        this.exaltedPlanets = exaltedPlanets;
+        this.exilPlanets = exilPlanets;
+        this.chutePlanets = chutePlanets;
     }
 
     public int getSignNumber() {
         return signNumber;
+    }
+
+    public Planet[] getMasterPlanets() {
+        return masterPlanets;
+    }
+
+    public Planet[] getExaltedPlanets() {
+        return exaltedPlanets;
+    }
+
+    public Planet[] getExilPlanets() {
+        return exilPlanets;
+    }
+
+    public Planet[] getChutePlanets() {
+        return chutePlanets;
     }
 
     private static List<Sign> signs = new ArrayList<Sign>(12);
