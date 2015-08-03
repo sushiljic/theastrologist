7ed5e66d87ec0ca2fb63aa4c52639b5e0c16f547
Samy Amirou <samyamirou@gmail.com>
2015-08-04 01:27:07 +0200

    le service marche test mock sur service d'appel de thème Reste une incohérence entre DateTime en TU et avec le serveur tomcat

---
+++ service/src/main/java/com/theastrologist/domain/DegreeJSONAdapter.java
@@ -0,0 +1,33 @@
+package com.theastrologist.domain;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+
+import java.io.IOException;
+
+/**
+ * Created by SAM on 03/08/2015.
+ */
+public class DegreeJSONAdapter extends TypeAdapter<Degree> {
+    @Override
+    public void write(JsonWriter out, Degree degree) throws IOException {
+        out.value(degree.toString());
+    }
+
+    @Override
+    public Degree read(JsonReader in) throws IOException {
+        // May be it came in formatted as a java.util.Date, so try that
+        Degree degree;
+        try {
+            double nextDouble = in.nextDouble();
+            degree = new Degree(nextDouble);
+        } catch (IOException ioe) {
+            String nextString = in.nextString();
+            degree = new Degree(nextString);
+        }
+        return degree;
+    }
+}
