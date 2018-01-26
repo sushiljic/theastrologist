c68827b65ae2048ca91e6d6a00f63c7900835f3d
Samy <samyamirou@gmail.com>
2018-01-26 17:57:49 +0100

    chore(unit-tests): Nettoyage dépendances Rest-Assured et EasyMock

--- rest/src/test/java/com/theastrologist/controller/TransitPeriodControllerTest.java
+++ rest/src/test/java/com/theastrologist/controller/TransitPeriodControllerTest.java
@@ -2,108 +2,132 @@ package com.theastrologist.controller;
 
 import com.theastrologist.domain.Degree;
 import com.theastrologist.util.TimeService;
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
-import io.restassured.module.mockmvc.response.MockMvcResponse;
 import org.joda.time.DateTimeZone;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.net.URISyntaxException;
-
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
-import static org.easymock.EasyMock.*;
-import static org.easymock.EasyMock.replay;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasSize;
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.*;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 /**
  * Created by Samy on 16/09/2015.
  */
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@WebMvcTest(TransitPeriodController.class)
 public class TransitPeriodControllerTest {
+	@Autowired
 	private TimeService timeService;
-	private TransitPeriodController transitPeriodController;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		MockitoAnnotations.initMocks(TransitPeriodController.class);
+	}
 
 	@Before
 	public void setUp() throws Exception {
-		transitPeriodController = new TransitPeriodController();
-		timeService = createMockBuilder(TimeService.class).addMockedMethod("queryGoogleForTimezone").createMock();
-		transitPeriodController.setTimeService(timeService);
-
-		RestAssuredMockMvc.standaloneSetup(transitPeriodController);
+		reset(timeService);
 	}
 
 	@Test
-	public void testRequest() throws URISyntaxException {
+	public void testRequest() throws Exception {
+		when(timeService.queryGoogleForTimezone(anyDouble(), anyDouble(), anyLong()))
+				.thenReturn(DateTimeZone.forID("Europe/Paris"));
 
-		expect(timeService.queryGoogleForTimezone(anyDouble(), anyDouble(), anyLong()))
-				.andReturn(DateTimeZone.forID("Europe/Paris"));
-		replay(timeService);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/{natalDate}/{latitude:.+}/{longitude:.+}/transitperiod/{startDate}/{endDate}",
+					 "1985-01-04T11:20:00",
+					 Double.toString(new Degree(48, 39).getBaseDegree()),
+					 Double.toString(new Degree(2, 25).getBaseDegree()),
+					 "2014-01-01T11:20:00",
+					 "2016-01-01T11:20:00");
 
-		MockMvcResponse response = get("/{natalDate}/{latitude:.+}/{longitude:.+}/transitperiod/{startDate}/{endDate}",
-									   "1985-01-04T11:20:00",
-									   Double.toString(new Degree(48, 39).getBaseDegree()),
-									   Double.toString(new Degree(2, 25).getBaseDegree()),
-									   "2014-01-01T11:20:00",
-									   "2016-01-01T11:20:00");
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String body = result.getResponse().getContentAsString();
+		MockHttpServletResponse response = result.getResponse();
 
-		response.then().statusCode(200)
-				.body("planetPeriods.PLUTON", hasSize(4))
-				.body("housePeriods.NOEUD_NORD_MOYEN", hasSize(2))
-				.body("housePeriods.MARS", hasSize(14));
-
-		verify(timeService);
+		assertThat(response.getStatus(), equalTo(HttpStatus.OK.value()));
+		assertThat(body, hasJsonPath("planetPeriods.PLUTON", hasSize(4)));
+		assertThat(body, hasJsonPath("housePeriods.NOEUD_NORD_MOYEN", hasSize(2)));
+		assertThat(body, hasJsonPath("housePeriods.MARS", hasSize(14)));
 	}
 
 	@Test
-	public void testRequestCompleterPeriodes() throws URISyntaxException {
+	public void testRequestCompleterPeriodes() throws Exception {
+		when(timeService.queryGoogleForTimezone(anyDouble(), anyDouble(), anyLong()))
+				.thenReturn(DateTimeZone.forID("Europe/Paris"));
 
-		expect(timeService.queryGoogleForTimezone(anyDouble(), anyDouble(), anyLong()))
-				.andReturn(DateTimeZone.forID("Europe/Paris"));
-		replay(timeService);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/{natalDate}/{latitude:.+}/{longitude:.+}/transitperiod/{startDate}/{endDate}",
+					 "1985-01-04T11:20:00",
+					 Double.toString(new Degree(48, 39).getBaseDegree()),
+					 Double.toString(new Degree(2, 25).getBaseDegree()),
+					 "2014-01-01T11:20:00",
+					 "2016-01-01T11:20:00");
 
-		MockMvcResponse response = get("/{natalDate}/{latitude:.+}/{longitude:.+}/transitperiod/{startDate}/{endDate}",
-									   "1985-01-04T11:20:00",
-									   Double.toString(new Degree(48, 39).getBaseDegree()),
-									   Double.toString(new Degree(2, 25).getBaseDegree()),
-									   "2014-01-01T11:20:00",
-									   "2016-01-01T11:20:00");
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String body = result.getResponse().getContentAsString();
+		MockHttpServletResponse response = result.getResponse();
 
-		response.then().statusCode(200)
-				.body("planetPeriods.PLUTON", hasSize(4))
-				.body("housePeriods.NOEUD_NORD_MOYEN", hasSize(2))
-				.body("housePeriods.MARS", hasSize(14));
-
-		verify(timeService);
+		assertThat(response.getStatus(), equalTo(HttpStatus.OK.value()));
+		assertThat(body, hasJsonPath("planetPeriods.PLUTON", hasSize(4)));
+		assertThat(body, hasJsonPath("housePeriods.NOEUD_NORD_MOYEN", hasSize(2)));
+		assertThat(body, hasJsonPath("housePeriods.MARS", hasSize(14)));
 	}
 
 	@Test
-	public void testRequestShortDates() throws URISyntaxException {
+	public void testRequestShortDates() throws Exception {
+		when(timeService.queryGoogleForTimezone(anyDouble(), anyDouble(), anyLong()))
+				.thenReturn(DateTimeZone.forID("Europe/Paris"));
 
-		expect(timeService.queryGoogleForTimezone(anyDouble(), anyDouble(), anyLong()))
-				.andReturn(DateTimeZone.forID("Europe/Paris"));
-		replay(timeService);
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/{natalDate}/{latitude:.+}/{longitude:.+}/transitperiod/{startDate}/{endDate}",
+					 "1985-01-04T11:20:00",
+					 Double.toString(new Degree(48, 39).getBaseDegree()),
+					 Double.toString(new Degree(2, 25).getBaseDegree()),
+					 "2014-01-01",
+					 "2016-01-01");
 
-		MockMvcResponse response = get("/{natalDate}/{latitude:.+}/{longitude:.+}/transitperiod/{startDate}/{endDate}",
-									   "1985-01-04T11:20:00",
-									   Double.toString(new Degree(48, 39).getBaseDegree()),
-									   Double.toString(new Degree(2, 25).getBaseDegree()),
-									   "2014-01-01",
-									   "2016-01-01");
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String body = result.getResponse().getContentAsString();
+		MockHttpServletResponse response = result.getResponse();
 
-		response.then().statusCode(200).body("planetPeriods.PLUTON", hasSize(4));
-
-		verify(timeService);
+		assertThat(response.getStatus(), equalTo(HttpStatus.OK.value()));
+		assertThat(body, hasJsonPath("planetPeriods.PLUTON", hasSize(4)));
 	}
 
 	@Test
-	public void testTransitPeriodRequestWithAddress() {
+	public void testTransitPeriodRequestWithAddress() throws Exception {
 
-		MockMvcResponse response = get("/{natalDate}/{address}/transitperiod/{startDate}/{endDate}",
-									   "1985-01-04T11:20:00", "Ris-Orangis", "2014-01-01", "2016-01-01");
+		RequestBuilder requestBuilder = MockMvcRequestBuilders
+				.get("/{natalDate}/{address}/transitperiod/{startDate}/{endDate}",
+					 "1985-01-04T11:20:00", "Ris-Orangis", "2014-01-01", "2016-01-01");
 
-		response.then().statusCode(200)
-				.body("planetPeriods.PLUTON", hasSize(5))
-				.body("housePeriods.NOEUD_NORD_MOYEN", hasSize(2))
-				.body("housePeriods.MARS", hasSize(14));
+		MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+		String body = result.getResponse().getContentAsString();
+		MockHttpServletResponse response = result.getResponse();
+
+		assertThat(response.getStatus(), equalTo(HttpStatus.OK.value()));
+		assertThat(body, hasJsonPath("planetPeriods.PLUTON", hasSize(4)));
+		assertThat(body, hasJsonPath("housePeriods.NOEUD_NORD_MOYEN", hasSize(2)));
+		assertThat(body, hasJsonPath("housePeriods.MARS", hasSize(14)));
 	}
 }
